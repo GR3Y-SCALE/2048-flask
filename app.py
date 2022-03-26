6feef7b6b8d9f32aae619835ee118f214d0563bc
GR3Y-SCALE <evanfoxx18@gmail.com>
2022-03-26 22:35:49 +1000

    adjusted requirements

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
-#!/usr/bin/env python
-from flask import Flask, render_template,request,url_for, jsonify
+from flask import Flask, render_template,request,url_for, jsonify, request
 import sqlite3
 from datetime import datetime
 import json
+from flask_table import Table, Col
 
+
+debugging = True
 
 app = Flask(__name__)
 
+class ItemTable(Table):
+    name = Col('Name')
+    description = Col('Description')
 
-@app.route('/', methods=["GET","POST"])
-def home():
-    winnerScore = 500
-    winnerName = ''
+# Get some objects
+class Item(object):
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
 
+items = [dict(name='Name1', description='Description1'),
+         dict(name='Name2', description='Description2'),
+         dict(name='Name3', description='Description3')]
+
+# Populate the table
+table = ItemTable(items)
+
+# Print the html
+print(table.__html__())
+# or just {{ table }} from within a Jinja template
+
+
+def dbinit():
     con = sqlite3.connect("leaderboard.db")
     query = con.cursor()
     query.execute('''CREATE TABLE IF NOT EXISTS record (
@@ -20,20 +39,37 @@ def home():
                     score int
                     );''')
     con.commit()
+    query.execute('''SELECT * FROM record;''')
+    records = query.fetchall()
+    if (debugging):
+        print('db initiated')
+
+def sendScoreToDB(winnerName, winnerScore):
     query.execute('''INSERT INTO record (name,score)
                     VALUES (?,?);''', (winnerName, winnerScore))
     con.commit()
-    query.execute('''SELECT * FROM record;''')
-    records = query.fetchall()
+    if (debugging):
+        print('stored: ' + winnerName + ' : ' + winnerScore)
 
-    return render_template("index.html")
+
+@app.route('/', methods=["GET"])
+def home():
+    dbinit()
+
+
+
+    return render_template("index.html", highScore = 10, table = table.__html__())
 
 
 
 @app.route('/score', methods=["POST"])
 def score():
-    received = request.get_json()
-    score = json.loads(received) #converts JSON into python dictionary
+    dict_values = request.get_json()
+    print(request)
+    print(dict_values)
+
+    return 'OK'
+
 
 
 
